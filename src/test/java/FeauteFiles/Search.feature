ecafdac6feb737864c051c04204492cdcc33d0ab
Sema <semayartasi@hotmail.com>
2020-08-20 11:10:00 -0400

    DataTable

--- src/test/java/FeauteFiles/Search.feature
+++ src/test/java/FeauteFiles/Search.feature
@@ -1,12 +1,12 @@
 Feature: Search in the website
-  Scenario Outline: Search Product
+  Scenario: Search Product
     Given Navigate to website
     When Open the website close popup
-    Then Search "<product>" and click search
+    When I search for products I expect number of results to be:
+      |product|results|
+      |imac 27"|7|
+      |ipad air 256gb|6|
+      |apple watch 5 protector|16|
+      |pastel case apple ipod|1|
 
-    Examples:
-    |product|
-    |ipad|
-    |imac|
-    |airpod|
 
--- src/test/java/StepDefitions/Search.java
+++ src/test/java/StepDefitions/Search.java
@@ -1,13 +1,21 @@
 package StepDefitions;
 
-import cucumber.api.PendingException;
+import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import gherkin.lexer.Th;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import utils.Driver;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class Search {
     WebDriver driver;
@@ -16,7 +24,6 @@ public class Search {
         driver = Driver.getDriver();
         driver.get("https://www.bestbuy.com/");
         driver.manage().window().maximize();
-
     }
 
     @When("^Open the website close popup$")
@@ -27,23 +34,32 @@ public class Search {
        }catch (Exception e){
            System.out.println("No pop up close");
        }
-
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("gh-search-input")));
     }
 
-    @Then("^Search \"([^\"]*)\" and click search$")
-    public void searchAndClickSearch(String product) throws InterruptedException {
-        Thread.sleep(1000);
-//        //search Ipad
-//        WebDriverWait wait = new WebDriverWait(driver, 10);
-//        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("input[class='search-input']"))));
-        WebElement search=driver.findElement(By.cssSelector("input[class='search-input']"));
-//        //WebElement search=driver.findElement(By.xpath("//input[@class='search-input']"));
-        search.sendKeys(product);
-        Thread.sleep(1000);
-        //        //click search button
-        WebElement clickSearchButton=driver.findElement(By.cssSelector("button[class='header-search-button']"));
-//       // WebElement clickSearchButton=driver.findElement(By.xpath("//button[@class='header-search-button']"));
-        clickSearchButton.click();
+    @Then("^I search for products I expect number of results to be:$")
+    public void searchAndClickSearch(DataTable table) throws InterruptedException {
+        List<Map<String, String>> mapList = table.asMaps(String.class, String.class);
+        for ( Map<String, String> row: mapList) {
+            Thread.sleep(300);
+        //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        //  WebDriverWait wait = new WebDriverWait(driver, 10);
+       //  wait.until(ExpectedConditions.presenceOfElementLocated(By.id("gh-search-input")));
+         //   wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("gh-search-input")));
+            WebElement search = driver.findElement(By.cssSelector("input.search-input"));
+            search.sendKeys(row.get("product"));
+//            wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("button[class='header-search-button']")));
+//            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[class='header-search-button']")));
+            driver.findElement(By.cssSelector("button[class='header-search-button']")).click();
+            Thread.sleep(3000);
+            int results = driver.findElements(By.cssSelector("li.sku-item")).size();
+             Thread.sleep(3000);
+            System.out.println(results);
+            Assert.assertEquals(results, Integer.parseInt(row.get("results")));
+            driver.findElement(By.cssSelector(".bby-logo-lv")).click();
+        }
+
 
     }
 
